a1017aeb72a5828bf53161f9bb21068a023421b7
erick <ericksotoe@gmail.com>
2020-03-01 20:56:32 -0800

    Began adding waves of enemies using waypoints need to add enemy shooting and different paths

---
+++ Galactic Wars/Assets/Scripts/Player.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    // config parameters
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 0.5f;
+    [SerializeField] GameObject lazerPrefab;
+    [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] float projectileFiringRate = 0.1f;
+
+    Coroutine firingCoroutine;
+
+    // cached references
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+
+    // Start is called before the first frame update
+    void Start() {
+        SetUpMoveBoundaries();
+    }
+
+
+
+    // Update is called once per frame
+    void Update() {
+        Move();
+        Fire();
+    }
+
+    // fire method is used to fire the lazer upwards by the pilot
+    private void Fire() {
+        // if the fire1(spacebar) is pressed create a lazer and shoot it upwards
+        if (Input.GetButtonDown("Fire1")) {
+            // sets it in a coroutine so we can stop it below
+            firingCoroutine = StartCoroutine(FireContinuously());
+        }
+        if (Input.GetButtonUp("Fire1")) {
+            StopCoroutine(firingCoroutine);
+        }
+    }
+
+    IEnumerator FireContinuously() {
+        while (true) {
+            GameObject lazer = Instantiate(lazerPrefab, transform.position, Quaternion.identity) as GameObject;
+            lazer.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+            yield return new WaitForSeconds(projectileFiringRate);
+        }
+    }
+
+    // grabs horizontal change that the player inputs using arrows or 'a' 's' keys and moves obj
+    private void Move() {
+        // we multiply by Time.deltaTime to make it framerate independent
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+
+    private void SetUpMoveBoundaries() {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+}
